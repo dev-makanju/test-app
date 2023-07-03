083934080e6f2b82b756866a1c4bd2ecb08e51c4
miracool <makurseme@gmail.com>
2023-07-03 13:29:01 +0100

    add scalfold

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Appearance, useColorScheme} from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+
+
+const HomeScreen = () => {
+  let [fontsLoaded] = useFonts({
+  Inter_900Black,
+      'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12', 
+  });
+  let colorScheme = useColorScheme();
+  //toggle between dark and 
+  const themeTextStyle = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer; 
+  const themeContainerStyle = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
+  //check if font is loaded
+  if(!fontsLoaded){
+    return  null
+  }
+
+  const insets = useSafeAreaInsets();
+  const styObj = {
+    paddingTop: insets.top,
+    themeContainerStyle,
+  }
+  
+  return (
+    <View style={{ ...styObj }} >
+        <Text style={[styles.text, themeTextStyle]}>Content is in safe area.. {colorScheme}</Text>
+        <StatusBar style="auto" />
+    </View>
+  )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <HomeScreen/>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   //okay...
+  },
+  lightContainer: {
+    backgroundColor: '#d0d0c0',
+  },
+  darkContainer: {
+    backgroundColor: '#242c40',
+  },
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#d0d0c0',
   },
 });
